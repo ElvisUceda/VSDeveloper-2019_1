46a0b6f1fe1ef651cc0e519fbc3d3a5bd714a89b
Soporte Tecnico UTI <os99st@foncodes.pe>
2019-02-13 18:46:45 -0500

    changeMiercoles

--- Cap02/slnApp/Chinook.Data.Test/GenreDATest.cs
+++ Cap02/slnApp/Chinook.Data.Test/GenreDATest.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Chinook.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Chinook.Data.Test
 {
-    class GenreDATest
+    [TestClass]
+    public class GenreDATest
     {
+        [TestMethod]
+        public void InsertGenreTest()
+        {
+            var da = new GenreDA();
+            var nuevoGenero = da.InsertGenre(
+                new Genre() { Name = "Nuevo Genero" + Guid.NewGuid().ToString() });
+
+            Assert.IsTrue(nuevoGenero > 0);
+
+        }
 
     }
 }
--- Cap02/slnApp/Chinook.Data/GenreDA.cs
+++ Cap02/slnApp/Chinook.Data/GenreDA.cs
@@ -7,68 +7,34 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 
-//namespace Chinook.Data
-//{
-//    public class GenreDA:BaseConnection 
-//    {
+namespace Chinook.Data
+{
+    public class GenreDA:BaseConnection 
+  {
 
-//        public int GetCount()
-//        {
-//            var result = 0;
+       
 
-//            var sql = "SELECT COUNT(1) FROM Genre";
-//            /*1: Create una instancia sql DbConnection*/
-//            using (IDbConnection cn = new SqlConnection(GetConnection()))
-//            {
-//                /*2: Create ua instancia de Command*/
-//                IDbCommand cmd = new SqlCommand(sql);
-//                cmd.Connection = cn;
-//                cn.Open(); //Abriendo la conexion a la DB
-//                           /*3. ejecutando el comando*/
-//                result = (int)cmd.ExecuteScalar();
-//            }
+        public int InsertGenre(Genre entity)
+        {
+            var result = 0;
+            using (IDbConnection cn
+                = new SqlConnection(GetConnection()))
+            {
+                cn.Open();
+                IDbCommand command =
+                    new SqlCommand("usp_InsertGenre");
+                command.Connection = cn;
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(
+                    new SqlParameter("@Name", entity.Name)
+                    );
 
-//            return result;
-//        }
+                result = Convert.ToInt32(command.ExecuteScalar());
 
-//        public List<Genre> GetGenresWithSP( string filterByname)
-//        {
-//            var result = new List<Genre>();
-//            var sql = "USP_GetGenre";
-//            using (IDbConnection cn = new SqlConnection(GetConnection()))
-//            {
-//                /*2: Create ua instancia de Command*/
-//                IDbCommand cmd = new SqlCommand(sql);
-//                cmd.CommandType = CommandType.StoredProcedure;
-//                cmd.Connection = cn;
+            }
 
-//                //Agregando el parametro
-//                cmd.Parameters.Add(new SqlParameter("@pNombre", filterByname));
+            return result;
 
-//                cn.Open(); //Abriendo la conexion a la DB
-//                           /*3. ejecutando el comando*/
-
-//                var indice = 0;
-//                var reader = cmd.ExecuteReader();
-//                while (reader.Read())
-//                {
-//                    indice = reader.GetOrdinal("GenreId");
-//                    var GenreId = reader.GetInt32(indice);
-
-//                    indice = reader.GetOrdinal("Name");
-//                    var name = reader.GetString(indice);
-
-//                    //result.Add(
-//                    //        new Genre()
-//                    //        {
-//                    //            GenreId = genreId,
-//                    //            Name = name
-//                    //        }
-//                    //    );
-//                }
-//            }
-
-//            return result;
-//        }
-//    }
-//}
+        }
+    }
+}
