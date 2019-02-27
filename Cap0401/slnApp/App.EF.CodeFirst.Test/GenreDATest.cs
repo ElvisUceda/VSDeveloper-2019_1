a910be16a1c9389d0743440964aa41b9184a955b
ElvisUceda <47511391+ElvisUceda@users.noreply.github.com>
2019-02-26 22:19:35 -0500

    Cambios002

---
+++ Cap0401/slnApp/App.EF.CodeFirst.Test/GenreDATest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.EF.CodeFirst.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.EF.CodeFirst.Test
+{
+    [TestClass]
+    public class GenreDATest
+    {
+        [TestMethod]
+        public void Count()
+        {
+            var da = new GenreDA();
+            Assert.IsTrue(da.Count() > 0);
+        }
+        [TestMethod]
+        public void Insert()
+        {
+            var da = new GenreDA();
+            var id = da.Insert(
+                new Genre()
+                {
+                    Name = "Genre_"
+                            + Guid.NewGuid().ToString()
+                }
+                );
+
+            Assert.IsTrue(id > 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteBatch()
+        {
+            var list = new List<int>();
+            list.Add(27);
+            list.Add(28);
+            list.Add(29);
+            var da = new GenreDA();
+            Assert.IsTrue(da.DeleteBatch(list));
+        }
+    }
+}
---
+++ Cap0401/slnApp/App.EF.CodeFirst.Test/TrackDATest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using App.EF.CodeFirst.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace App.EF.CodeFirst.Test
+{
+    [TestClass]
+    public class TrackDATest
+    {
+        [TestMethod]
+        public void GetTracksByNameLinQ()
+        {
+            var da = new TrackDA();
+            Assert.IsTrue(da.GetTrackByNameLinQ("Aero").Count() > 0);
+        }
+
+    }
+}
