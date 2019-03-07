e874f7718cefb35a99276cb526244e522a984a0b
ElvisUceda <47511391+ElvisUceda@users.noreply.github.com>
2019-03-07 12:55:35 -0500

    Update usp_GetArtist.sql

--- Cap06/database/sp/usp_GetArtist.sql
+++ Cap06/database/sp/usp_GetArtist.sql
@@ -225,24 +225,24 @@ BEGIN
 	WHERE FirstName = @Firstname
 END
 GO
-CREATE PROCEDURE usp_GetTrack
-(
-@name NVARCHAR(200)
-)
-AS
-BEGIN
-SELECT      A.TrackId, A.Name as TrackName,
-	A.AlbumId, B.Title, A.MediaTypeId,
-	 F.Name AS MediaTypeName, A.GenreId, D.Name AS GenreName, B.ArtistId, 
-            C.Name AS ArtistName
-FROM        dbo.Track AS A INNER JOIN
-    dbo.Album AS B ON A.AlbumId =  B.AlbumId INNER JOIN
-    dbo.Artist AS C ON B.ArtistId = C.ArtistId INNER JOIN
-    dbo.Genre AS D ON A.GenreId = D.GenreId INNER JOIN
-    dbo.MediaType AS F ON A.MediaTypeId = F.MediaTypeId
+//CREATE PROCEDURE usp_GetTrack
+//(
+//@name NVARCHAR(200)
+//)
+//AS
+//BEGIN
+//SELECT      A.TrackId, A.Name as TrackName,
+//	A.AlbumId, B.Title, A.MediaTypeId,
+//	 F.Name AS MediaTypeName, A.GenreId, D.Name AS GenreName, B.ArtistId, 
+//            C.Name AS ArtistName
+//FROM        dbo.Track AS A INNER JOIN
+ //   dbo.Album AS B ON A.AlbumId =  B.AlbumId INNER JOIN
+  //  dbo.Artist AS C ON B.ArtistId = C.ArtistId INNER JOIN
+  //  dbo.Genre AS D ON A.GenreId = D.GenreId INNER JOIN
+  //  dbo.MediaType AS F ON A.MediaTypeId = F.MediaTypeId
 
-END
- exec usp_GetTrack '%'
+//END
+ //exec usp_GetTrack '%'
 
 
 CREATE PROCEDURE usp_GeTracks
