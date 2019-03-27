4e2d58bfcdd2cfe6d4accfa71b4b213f1b270497
ElvisUceda <47511391+ElvisUceda@users.noreply.github.com>
2019-03-26 22:24:25 -0500

    WebForm

---
+++ Cap09/slnApp/App.Domain/ArtistDomain.cs
@@ -0,0 +1,28 @@
+﻿using App.DataAccess.Repository;
+using App.Domain.Interfaces;
+using App.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain
+{
+    public class ArtistDomain : IArtistDomain
+    {
+        public IEnumerable<Artist> GetArtists( string nombre)
+        {
+            IEnumerable<Artist> result = new List<Artist>();
+
+            using(var uw = new AppUnitOfWork())
+            {
+                result = uw.ArtistRepository.GetAll(
+                        item=>item.Name.Contains(nombre)
+                        );
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Cap09/slnApp/App.Service.WCFAppTest/TrackReportesTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Service.WCFAppTest
+{
+    //[TestClass]
+    //public class TrackReportesTest
+    //{
+    //    [TestMethod]
+    //    public void GetTrackAll()
+    //    {
+    //        var client = new ReportesServices.ReportesServicesClient();
+
+    //        var lista = client.GetTrackAlls("%AC%");
+
+    //        Assert.IsTrue(lista.Count > 0);
+    //    }
+   // }
+    
+}
