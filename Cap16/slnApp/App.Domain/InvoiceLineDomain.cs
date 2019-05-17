60684f0ad64aaafa7c070f5c26f38991859fc7f8
ElvisUceda <47511391+ElvisUceda@users.noreply.github.com>
2019-05-16 21:55:33 -0500

    Commit Reportes

---
+++ Cap16/slnApp/App.Domain/InvoiceLineDomain.cs
@@ -0,0 +1,47 @@
+﻿using App.DataAccess.Repository;
+using App.Domain.Interfaces;
+using App.Entities.Base;
+using App.Entities.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain
+{
+    public class InvoiceLineDomain : IInvoiceLineDomain
+    {
+        public bool SaveInvoiceLine(InvoiceLine invoiceLine)
+        {
+            var result = false;
+
+            try
+            {
+                using (var uw = new AppUnitOfWork())
+                {
+                    /*Editando InvoiceLine*/
+                    if (invoiceLine.InvoiceLineId > 0)
+                    {
+                        uw.InvoiceLineRepository.Update(invoiceLine);
+                    }
+                    else 
+                    {
+                        uw.InvoiceLineRepository.Add(invoiceLine);
+                    }
+
+                    result = uw.Complete() > 0;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Agregar el exeption aun log de errores
+            }
+
+            return result;
+       
+
+        }
+    }
+}
---
+++ Cap16/slnApp/App.Service.WCFAppTest/InvoiceServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using App.Entities.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Service.WCFAppTest
+{
+    [TestClass]
+    public class InvoiceServiceTest
+    {
+        [TestMethod]
+        public void SaveInvoice()
+        {
+            var invoiceTest = new Invoice();
+            invoiceTest.CustomerId = 1;
+            invoiceTest.InvoiceDate = DateTime.Now;
+            invoiceTest.InvoiceLine = new List<InvoiceLine>();
+            invoiceTest.InvoiceLine.Add(
+                new InvoiceLine()
+                {
+                    Quantity = 12,
+                    TrackId = 1,
+                    UnitPrice = 10
+                }
+                );
+
+        }
+    }
+}
